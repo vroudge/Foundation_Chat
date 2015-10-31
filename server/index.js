2a6b70c0a089d7295136222709a92a31f3a9888d
V.Roudge <v.roudge@gmail.com>
2015-10-31 08:34:33 +0100

    initial

--- server/index.js
+++ server/index.js
@@ -1,3 +1,19 @@
 /**
  * Created by Psykopatik on 31/10/2015.
  */
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
